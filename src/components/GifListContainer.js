a08a6bf0ba84ed95349068b14097cb0db8c04ca7
+ <+>
2023-01-23 11:07:20 +0300

    completed assignment

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import GifSearch from "./GifSearch";
+import GifList from "./GifList";
+// import "./App.css";
+import { useState, useEffect } from "react";
+
+function GifListContainer (){
+    const[gif, setGif] = useState([])
+    const[search, setSearch] = useState("")
+    // provide personal API key to a variable
+    const myApiKey= "8dnjIQswNVaihW9B296nmf9g9EfBtIZg";
+
+    useEffect(()=>{
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${myApiKey}&rating=g`)
+        .then(response => response.json())
+        // .then((data)=> console.log(data))})
+        .then(({data}) => {
+            const gifs = data.slice(0,3).map((gif)=>({url: gif.images.original.url }));
+            setGif(gifs);
+        })
+
+    }, [search])
+
+
+    return(
+        <>
+            <GifSearch search={setSearch} />
+            <GifList gifs={gif}/>
+        </>
+    )
+}
+
+export default GifListContainer;
